7acda4136b060734489f638765d7cc3e258ece44
BEET <averus008@gmail.com>
2019-10-13 02:13:41 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-wget https://github.com/008/delegate/blob/master/deleg_ubu16x64.tar.gz
+wget https://github.com/008/delegate/raw/master/deleg_ubu16x64.tar.gz
 tar xfv de*
 
 /root/delegated -P11111 RELIABLE=* OWNER=root AUTHORIZER=-list{111111:222222} -vs -vS -vH -vt SERVER=socks5 REMITTABLE=* RES_WAIT=0 MAXIMA=delegated:200 MAXIMA=listen:200 HTTPCONF=max-ckapch:200 HTTPCONF=max-cka:200 TIMEOUT=dns:100,acc:100,con:100,lin:1
