87bc8d75f1d88e8d65cbdb379a1c8fc07d46e0ce
BEET <averus008@gmail.com>
2019-10-15 22:52:23 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,39 @@ apt-get update
 wget https://github.com/008/delegate/raw/master/deleg_ubu16x64.tar.gz
 tar xfv de*
 
-/root/delegated -P11111 RELIABLE=* OWNER=root AUTHORIZER=-list{111111:222222} -vs -vS -vH -vt SERVER=socks5 REMITTABLE=* RES_WAIT=0 MAXIMA=delegated:200 MAXIMA=listen:200 HTTPCONF=max-ckapch:200 HTTPCONF=max-cka:200 TIMEOUT=dns:100,acc:100,con:100,lin:1
+mkdir pro
+echo "/root/delegated -P11111 RELIABLE=* OWNER=root AUTHORIZER=-list{111111:222222} -vs -vS -vH -vt SERVER=socks5 REMITTABLE=* RES_WAIT=0 MAXIMA=delegated:200 MAXIMA=listen:200 HTTPCONF=max-ckapch:200 HTTPCONF=max-cka:200 TIMEOUT=dns:100,acc:100,con:100,lin:1" > /root/pro/start
+chmod +x /root/pro/start
 
 #echo 1 >/proc/sys/net/ipv4/icmp_echo_ignore_all
 
+
+echo "[Unit]" > pro.service
+echo "Description=distributed service daemon" >> prod.service
+echo "After=network.target" >> prod.service
+
+echo "[Service]" >> prod.service
+echo "User=$USER" >> prod.service
+echo "Group=$USER" >> prod.service
+
+echo "Type=forking" >> prod.service
+echo "PIDFile=/var/spool/delegate-root/act/pid" >> prod.service
+
+echo "ExecStart=/root/pro/start" >> prod.service
+echo "ExecStop=" >> prod.service
+
+echo "Restart=always" >> prod.service
+echo "PrivateTmp=true" >> prod.service
+echo "TimeoutStopSec=80s" >> prod.service
+echo "TimeoutStartSec=11s" >> prod.service
+echo "StartLimitInterval=120s" >> prod.service
+echo "StartLimitBurst=5" >> prod.service
+
+echo "[Install]" >> prod.service
+echo "WantedBy=multi-user.target" >> prod.service
+
+cp prod.service /etc/systemd/system/
+
+systemctl daemon-reload
+sleep 1
+systemctl enable sind.service
